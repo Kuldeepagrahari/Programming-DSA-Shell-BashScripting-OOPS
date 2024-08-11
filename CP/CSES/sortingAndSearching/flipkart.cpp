50a525bd53c99b66d5fcc4fbc992d754c99d3eec
Kuldeepagrahari <kuldeepagrahari9103@gmail.com>
2024-08-11 15:03:56 +0530

    kabhi kabhi cp bhi kr raha hu

---
+++ CP/CSES/sortingAndSearching/flipkart.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using  namespace std;
+int main(){
+    int a;
+    static float b = 3.2;
+    string str ="ap";
+    cout << 5*str << endl;
+
+    // cout <<( floor(b) + --b) << endl;
+
+}
