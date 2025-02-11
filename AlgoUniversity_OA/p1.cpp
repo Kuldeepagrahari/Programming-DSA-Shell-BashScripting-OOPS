fba1de0de896a616b8d553f4d42c6932448eae1d
Kuldeepagrahari <kuldeepagrahari9103@gmail.com>
2025-02-11 15:39:26 +0530

    AAj maine Problem Solving ko Puri Nistha aur bina sansay ke apni karm bhumi maana hu, JAi Shree Ganesh! Jai Shree krishna!

--- AlgoUniversity_OA/p1.cpp
+++ AlgoUniversity_OA/p1.cpp
@@ -8,20 +8,22 @@ signed main(){
      while ( t-- ){
        int n;
        cin >> n;
-       int m, k;
-       cin >> m >> k;
-       string s;
-       cin >> s;
-       int c0 = 0;
-       int ans = 0;
-       for ( int i = 0 ; i < n; i++ ){
-        if ( s[i] == '0')c0++;
-        else c0 = 0;
-        if ( c0 == m ){
-            i += (k-1);
-            ans ++;
-            c0 = 0;
+       vector<int> v(n);
+       vector<int> v2(n);
+       for ( int i = 0 ;i  < n ; i ++ ) cin >> v[i];
+       for ( int i = 0 ;i  < n ; i ++ ) cin >> v2[i];
+
+       sort ( v.begin(), v.end());
+       sort ( v2.begin(), v2.end(), greater<int>());
+
+       set<int> st;
+       for ( int i = 0 ;i  < n ; i ++ ) {
+        for ( int j = 0 ; j < n ; j ++ ){
+          st.insert(v[i] + v2[j]);
         }
-       }cout << ans << endl;
+       }
+       if ( st.size() >= 3 )cout << "YES\n";
+       else cout << "NO\n";
+
      }
 }
